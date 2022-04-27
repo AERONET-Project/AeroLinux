639e96570347fdd1a73de9a0ee58c1ba95618af7
Anthony LaRosa <68565251+anthony-larosa@users.noreply.github.com>
2022-04-27 07:02:19 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -6,6 +6,7 @@
 # | The script is meant to run once, running again will add new user|
 # | Author: anthony.d.larosa@nasa.gov				    |
 #  -----------------------------------------------------------------
+user_var=$(logname)
 
 if [ `id -u` -ne 0 ]; then
 	echo "Please execute this installer script with sudo, exiting.."
@@ -36,39 +37,16 @@ else
 	echo "Sucessfully installed Hologram SDK"
 fi
 curl -L hologram.io/python-install | bash
-#Create new user via I/O
-read -p "Enter a new username:" user_var # Save username as var user_var
-echo "The current username is:" "$user_var"
 
-adduser --force-badname "$user_var" --gecos "" --disabled-password
-passwd "$user_var"
-while [ $? -ne 0 ]; do
-	sleep 3 #give time to ctrl-c
-	passwd $user_var
-done
-
-for GROUP in $(groups pi | sed 's/.*:\spi//'); do adduser "$user_var" $GROUP; done
-
-sleep 2
 getent group sudo | grep -q "$user_var"
 if [ $? -eq 0 ]; then
 	echo "$user_var has root privileges, continuing..."
 else
 	echo "Adding using to root failed...Try a new username?" 1>&2
-	userdel -r "$user_var"
 	exit 1
 fi
 
-autologinf=/etc/systemd/system/getty@tty1.service.d/autologin.conf
 
-if [ -e "$autologinf" ]; then
-	echo "An auto-login config file already exists"
-else
-	echo "Building an auto-login config file for "$user_var""
-	echo "[Service]"\ >> $autologinf
-	echo "ExecStart="\ >> $autologinf
-	echo "ExecStart=-/sbin/agetty --autologin "$user_var" --noclear %I $TERM" >> $autologinf
-fi
 
 echo "$user_var	ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 echo "Adding cronjobs to user's crontab"
@@ -90,7 +68,6 @@ mkdir /home/$user_var/logs #Make a log file directory
 mkdir /home/$user_var/backup #For data files saved to disk
 touch /home/$user_var/logs/connection.log
 touch /home/$user_var/logs/modem_diagnostics.log
-cp -r $PWD /home/$user_var #Copy the programs from current user to new user
 chown -R ${user_var}: /home/$user_var/
 chmod -R 777 /home/$user_var/
 
