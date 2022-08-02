cd444ac219f350566f152f81c2d878d2fc02de64
Anthony LaRosa <68565251+anthony-larosa@users.noreply.github.com>
2022-08-02 16:42:18 -0400

    Create backup_upload.sh

---
+++ controls/backup_upload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+modem_check=$(lsusb | grep Qualcomm) #returns string if modem was identified
+user_var=$(logname)
+
+if [ -z "$modem_check" ]; then #Checks if modem_check is 0/empty - i.e., no modem 
+	echo "A USB modem is not connected"	
+	/home/$user_var/aerolinux/controls/backup_send 60
+	
+else #Sees that modem_check has value and proceeds as modem is connected
+	echo "A USB modem is connected"
+	sudo hologram network connect
+	/home/$user_var/aerolinux/controls/backup_send 60
+	sudo hologram network disconnect
+fi
