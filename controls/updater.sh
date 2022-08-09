b808f7a9cdb483df2a9778a32cec90c5e2052cc8
Anthony LaRosa <68565251+anthony-larosa@users.noreply.github.com>
2022-08-09 08:41:10 -0400

    Update updater.sh
    Updated paths to: AeroLinux from aerolinux

--- controls/updater.sh
+++ controls/updater.sh
@@ -9,7 +9,7 @@ else
   sudo hologram network connect
 fi
 
-cd /home/$USER/aerolinux
+cd /home/$USER/AeroLinux
 if git fetch origin main --progress 2>&1 | grep -q "Enumerating"; then
   echo "Update found from GitHub, recompiling and then rebooting..."
   git fetch origin main
@@ -19,20 +19,20 @@ if git fetch origin main --progress 2>&1 | grep -q "Enumerating"; then
   echo "======================="
   sleep 3
 
-  cd /home/$USER/aerolinux/cimel_connect
+  cd /home/$USER/AeroLinux/cimel_connect
   
   cc -o models_connect_and_reset models_connect_and_reset.c models_port.c -lm -lcurl
   sleep 4
-  cd /home/$user_var/aerolinux/controls/
-  cc -o pi_ftp_upload /home/$user_var/aerolinux/controls/pi_ftp_upload.c -lm -lcurl
-  cc -o backup_send /home/$user_var/aerolinux/controls/find_and_upload_backup_files.c models_port.c -lm -lcurl
+  cd /home/$user_var/AeroLinux/controls/
+  cc -o pi_ftp_upload /home/$user_var/AeroLinux/controls/pi_ftp_upload.c -lm -lcurl
+  cc -o backup_send /home/$user_var/AeroLinux/controls/find_and_upload_backup_files.c models_port.c -lm -lcurl
  
   echo "Recompiled and now setting permissions"
-  chmod -R 777 /home/$USER/aerolinux/
-  chmod -R 777 /home/$USER/aerolinux/controls
-  chmod -R 777 /home/$USER/aerolinux/tools
-  chmod -R 777 /home/$USER/aerolinux/cimel_connect
-  chown -R ${USER}:${USER} /home/$USER/aerolinux/
+  chmod -R 777 /home/$USER/AeroLinux/
+  chmod -R 777 /home/$USER/AeroLinux/controls
+  chmod -R 777 /home/$USER/AeroLinux/tools
+  chmod -R 777 /home/$USER/AeroLinux/cimel_connect
+  chown -R ${USER}:${USER} /home/$USER/AeroLinux/
 
   if [ -z "$modem_check" ]; then
     echo "non-modem device"
