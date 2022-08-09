401a3965ec206daa2f581c84e953fa1e231ed554
Anthony LaRosa <68565251+anthony-larosa@users.noreply.github.com>
2022-08-09 08:43:40 -0400

    Update startup.sh
    path update

--- controls/startup.sh
+++ controls/startup.sh
@@ -2,7 +2,7 @@
 
 modem_check=$(lsusb | grep Qualcomm)
 if [ -z "$modem_check" ]; then
-	$HOME/aerolinux/cimel_connect/models_connect_and_reset </dev/null
+	$HOME/AeroLinux/cimel_connect/models_connect_and_reset </dev/null
 else
-	lsusb | $HOME/aerolinux/cimel_connect/models_connect_and_reset
+	lsusb | $HOME/AeroLinux/cimel_connect/models_connect_and_reset
 fi
